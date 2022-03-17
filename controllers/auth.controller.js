f445d2c1132457147c417b368fcd15ea33aaabc5
Ashutosh Dhande <ashutosh.dhande001@gmail.com>
2022-03-17 23:39:47 +0530

    User login successful

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -46,10 +46,43 @@ exports.signup = async (req, res) => {
     res.status(200).json(user);
   } catch (err) {
     console.error(err);
+    res.status(500).send("Something went wrong.");
   }
-  console.log(`Sign up is running`);
 };
 
-exports.signin = (req, res) => {
-  console.log(`Sign in is running`);
+exports.signin = async (req, res) => {
+  try {
+    //Grab fields & check if it exists
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      res
+        .status(400)
+        .send("<h1>All Fields are required. Check and Try again</h1>");
+    }
+
+    //find the user in DB
+    const user = await User.findOne({ email });
+
+    if (!user) res.status(400).send("Invalid Credentials");
+
+    //Check if password matches
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!(email && validPassword)) {
+      res.status(400).send("Invalid Credentials");
+    }
+
+    //Generate token
+    const token = jwt.sign(
+      { userId: user._id, email },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+    user.token = token;
+    user.password = undefined;
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong.");
+  }
 };
